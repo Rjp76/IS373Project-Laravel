74ef7b9230ea2d84176b027b64110eac7606d3b4
Mike <mike.nichols97@gmail.com>
2019-05-16 20:59:57 -0400

    Added: List of questions to the homepage and pager

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -16,13 +17,10 @@ class HomeController extends Controller
         $this->middleware('auth');
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
-        return view('home');
+        $user = Auth::user();
+        $questions = $user->questions()->paginate(6);
+        return view('home')->with('questions', $questions);
     }
 }
